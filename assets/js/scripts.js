6e5c811404c9dfc2f93b670b4cada20bd5148897
Sunn-y <in.wise.nature@gmail.com>
2020-12-15 03:36:26 +0900

    feat: responsive main

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,20 @@
+// $(function(){
+    function calc_width(){
+        var window_width = $(window).innerWidth();
+        var nav_width = $("nav").is(':visible')? parseInt($("nav").css("width").replaceAll("px","")) : 0;
+        var main_padding = (window_width - nav_width) * 0.05;
+        var main_width = (window_width - nav_width) - (main_padding * 2);
+        
+        $("main").css("left",nav_width).css("width", main_width).css("padding-left",main_padding).css("padding-right",main_padding);
+    }
+// });
+
 $(document).ready(function() {
+    //set
+    if($(window).innerWidth() < 1200){
+        $("nav").hide().addClass("is_hide"); 
+    }
+    calc_width();
     //animation
     if($(".container").hasClass('fadeOut')) {
         $(".container").removeClass("fadeOut").addClass("fadeIn");
@@ -6,34 +22,26 @@ $(document).ready(function() {
     if($(".wrapper").hasClass('fadeOut')) {
         $(".wrapper").removeClass("fadeOut").addClass("fadeIn");
     };
-    //nav
-    if($(window).innerWidth() < 1200){
-        $("nav").hide().addClass("is_hide"); 
-    }
-    if(!$("nav").hasClass("is_hide")){
-        $("main").css("left",360).css("width",$(window).innerWidth()-360);
-    }
+ 
     //responsive object
     $(window).resize(function(){
+       calc_width();     
         if($(window).innerWidth() < 1200){
             $("nav").hide(); 
-            $("main").css("width",$(window).innerWidth()).css("left",0);
         }
         if($(window).innerWidth() >= 1200){
             $("nav").show();
-            $("main").css("width",$(window).innerWidth()-360).css("left",360);
         }
     });
     //event
     $(".nav_toggle").click(function(){
         if($("nav").hasClass('is_hide')) {
             $("nav").removeClass("is_hide").show().addClass("fadeIn");
-            $("main").css("width",$(window).innerWidth()-360).css("left",360);
         }
         else{
             $("nav").removeClass("fadeIn").hide().addClass("is_hide");
-            $("main").css("width",$(window).innerWidth()).css("left",0);
         }
+        calc_width();
     });
     $('.goup').click(function() {
         $('html, body').animate({scrollTop: 0}, 500);
