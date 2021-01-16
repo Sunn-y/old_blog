999273b45429dd161547f8fd4c46e0d93042655d
Sunn-y <in.wise.nature@gmail.com>
2021-01-16 16:40:35 +0900

    renewal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,17 +15,10 @@ $(document).ready(function () {
     .each(function (index, item) {
       if (now.indexOf($(this).attr("href")) >= 0) {
         $(this).parent().parent().show();
-        $(this).css("text-decoration", "underline");
+        $(this).css("text-shadow", "1px 1px 5px $color2");
         return false;
       }
     });
-
-  //responsive object
-  // $(window).resize(function () {
-  //   if ($(window).innerWidth() >= 1200) {
-  //     $("nav").show();
-  //   }
-  // });
 
   // navagation sub menu toggle
   $("input[type=checkbox").change(function () {
@@ -42,7 +35,7 @@ $(document).ready(function () {
     if ($("nav").css("display") == "none") {
       $("nav").show();
       $(".main-container").css("width", "calc(100% - 250px)");
-      $(".nav_toggle").css("left", "239px");
+      $(".nav_toggle").css("left", "245px");
     } else {
       $("nav").hide();
       $(".main-container").css("width", "100%");
@@ -51,14 +44,14 @@ $(document).ready(function () {
   });
 
   // scroll event
-  $(window).scroll(function () {
+  $(".main-content").scroll(function () {
     $(".goup").show();
-    if ($(document).scrollTop() === 0) {
+    if ($(".main-content").scrollTop() === 0) {
       $(".goup").hide();
     }
   });
   $(".goup").click(function () {
-    $("html, body").animate(
+    $(".main-content").animate(
       {
         scrollTop: 0,
       },
